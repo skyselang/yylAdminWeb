62e4df0455dfeef2ec21112ea9d3d5feab6cf083
skyselang <215817969@qq.com>
2020-08-14 14:12:09 +0800

    eslintrc.js
    Signed-off-by: skyselang <215817969@qq.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// https://eslint.org/
 module.exports = {
   root: true,
   parserOptions: {
@@ -10,7 +11,6 @@ module.exports = {
     es6: true
   },
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
-
   // add your custom rules here
   // it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
