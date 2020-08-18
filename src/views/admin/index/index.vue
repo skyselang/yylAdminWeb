24aa4ddf4111813ff599b48714ec651e8981d989
skyselang <215817969@qq.com>
2020-08-18 17:39:06 +0800

    返回顶部
    Signed-off-by: skyselang <215817969@qq.com>

--- src/views/admin/index/index.vue
+++ src/views/admin/index/index.vue
@@ -5,15 +5,17 @@
         <Visit />
       </el-col>
     </el-row>
+    <back-to-top transition-name="fade" />
   </div>
 </template>
 
 <script>
+import BackToTop from '@/components/BackToTop'
 import Visit from '../visit/visit' // 访问统计
 
 export default {
   name: 'Dashboard',
-  components: { Visit },
+  components: { Visit, BackToTop },
   data() {
     return {}
   },
