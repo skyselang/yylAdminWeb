948dc7454a51a4ac93781d862692e92724bf7aa6
skyselang <215817969@qq.com>
2020-08-18 17:55:26 +0800

    工具
    Signed-off-by: skyselang <215817969@qq.com>

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -4,7 +4,7 @@ const title = defaultSettings.title || 'yylAdmin'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
-    return `${title}-${pageTitle}`
+    return `${title} - ${pageTitle}`
   }
   return `${title}`
 }
