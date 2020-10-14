8a4685717fd299b16c4a1be9389d2e512fd7c790
skyselang <215817969@qq.com>
2020-10-14 23:56:48 +0800

    401页面
    Signed-off-by: skyselang <215817969@qq.com>

--- src/views/admin/error/401.vue
+++ src/views/admin/error/401.vue
@@ -1,50 +1,35 @@
 <template>
-  <div class="errPage-container">
-    <el-button class="pan-back-btn" icon="el-icon-arrow-left" @click="back">
-      返回
-    </el-button>
+  <div class="app-container">
     <el-row>
-      <el-col :span="12">
-        <h1 class="text-jumbo text-ginormous">
-          Oops!
-        </h1>
-        <h2>你没有权限去该页面</h2>
-        <ul class="list-unstyled">
-          <li>或者你可以去:</li>
-          <li class="link-type">
-            <router-link to="/dashboard">
-              回控制台
-            </router-link>
-          </li>
-          <li class="link-type">
-            <a href="https://www.taobao.com/" target="_blank">随便看看</a>
-          </li>
-          <li>
-            <a href="#" @click.prevent="dialogVisible = true">点我看图</a>
-          </li>
-        </ul>
-      </el-col>
-      <el-col :span="12">
-        <img :src="errGif" width="313" height="428" alt="Girl has dropped her ice cream.">
+      <el-col :span="12" :offset="6" class="yyl-center yyl-top">
+        <el-image style="width:600px" :src="error401" fit="contain" />
       </el-col>
     </el-row>
-    <el-dialog :visible.sync="dialogVisible" title="随便看">
-      <img :src="ewizardClap" class="pan-img">
-    </el-dialog>
+    <el-row>
+      <el-col :span="6" :offset="6" class="yyl-center yyl-top">
+        <el-button icon="el-icon-arrow-left" @click="back">返回</el-button>
+      </el-col>
+      <el-col :span="6" class="yyl-center yyl-top">
+        <el-button icon="el-icon-s-home" @click="dashboard">控制台</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12" :offset="6" class="yyl-center yyl-top">
+        <a href="https://gitee.com/skyselang/yylAdmin" target="_blank" title="">
+          <img src="https://gitee.com/skyselang/yylAdmin/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b" alt="" srcset="">
+        </a>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import errGif from '@/assets/401_images/401.gif'
-
+import error401 from '@/assets/error/401.jpg'
 export default {
   name: 'Error401',
   data() {
     return {
-      errGif: errGif + '?' + +new Date(),
-      ewizardClap:
-        'https://wpimg.wallstcn.com/007ef517-bafd-4066-aae4-6883632d9646',
-      dialogVisible: false
+      error401: error401
     }
   },
   methods: {
@@ -54,47 +39,15 @@ export default {
       } else {
         this.$router.go(-1)
       }
+    },
+    dashboard() {
+      this.$router.push({ path: '/dashboard' })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.errPage-container {
-  width: 800px;
-  max-width: 100%;
-  margin: 100px auto;
-  .pan-back-btn {
-    background: #008489;
-    color: #fff;
-    border: none !important;
-  }
-  .pan-gif {
-    margin: 0 auto;
-    display: block;
-  }
-  .pan-img {
-    display: block;
-    margin: 0 auto;
-    width: 100%;
-  }
-  .text-jumbo {
-    font-size: 60px;
-    font-weight: 700;
-    color: #484848;
-  }
-  .list-unstyled {
-    font-size: 14px;
-    li {
-      padding-bottom: 5px;
-    }
-    a {
-      color: #008489;
-      text-decoration: none;
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-  }
-}
+.yyl-top{margin-top: 2vh;}
+.yyl-center { text-align: center;}
 </style>
