388d75278e45ba98cc4e06c3a67e6fded1a0e5f6
skyselang <215817969@qq.com>
2022-01-18 15:25:31 +0800

    修复：权限判断，当roles=admin时显示所有侧边栏菜单问题
    Signed-off-by: skyselang <215817969@qq.com>

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -49,13 +49,8 @@ const mutations = {
 const actions = {
   generateRoutes({ commit }, roles) {
     return new Promise(resolve => {
-      let accessedRoutes
-      if (roles.includes('admin')) {
-        accessedRoutes = asyncRoutes || []
-      } else {
-        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
-      }
-      commit('SET_ROUTES', accessedRoutes)
+      const accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+      commit('SET_ROUTES', accessedRoutes || [])
       resolve(accessedRoutes)
     })
   }
