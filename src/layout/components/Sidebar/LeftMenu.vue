162501a41d9d48491db6972cb60645841e657e7e
yuanlin <215817969@qq.com>
2025-06-28 17:12:30 +0800

    feat(LeftMenu): 为侧边栏菜单添加滚动条支持并支持混合布局样式
    为LeftMenu组件添加el-scrollbar包装，支持菜单内容滚动。新增mixLeftMenu计算属性判断是否为混合布局，并添加对应的样式类left-menu-mix，在混合布局时增加底部内边距和背景色。

--- src/layout/components/Sidebar/LeftMenu.vue
+++ src/layout/components/Sidebar/LeftMenu.vue
@@ -1,22 +1,24 @@
 <template>
-  <el-menu
-    :default-active="currRoute.path"
-    :collapse="!appStore.sidebar.opened"
-    :background-color="variables.menuBg"
-    :text-color="variables.menuText"
-    :active-text-color="variables.menuActiveText"
-    :unique-opened="false"
-    :collapse-transition="false"
-    :mode="layout === 'top' ? 'horizontal' : 'vertical'"
-  >
-    <sidebar-item
-      v-for="route in menuList"
-      :key="route.path"
-      :item="route"
-      :base-path="resolvePath(route.path)"
-      :is-collapse="!appStore.sidebar.opened"
-    />
-  </el-menu>
+  <el-scrollbar :class="{ 'left-menu-mix': mixLeftMenu }">
+    <el-menu
+      :default-active="currRoute.path"
+      :collapse="!appStore.sidebar.opened"
+      :background-color="variables.menuBg"
+      :text-color="variables.menuText"
+      :active-text-color="variables.menuActiveText"
+      :unique-opened="false"
+      :collapse-transition="false"
+      :mode="layout === 'top' ? 'horizontal' : 'vertical'"
+    >
+      <sidebar-item
+        v-for="route in menuList"
+        :key="route.path"
+        :item="route"
+        :base-path="resolvePath(route.path)"
+        :is-collapse="!appStore.sidebar.opened"
+      />
+    </el-menu>
+  </el-scrollbar>
 </template>
 
 <script setup>
@@ -32,6 +34,7 @@ const settingsStore = useSettingsStore()
 const appStore = useAppStore()
 const currRoute = useRoute()
 const layout = computed(() => settingsStore.layout)
+const mixLeftMenu = computed(() => settingsStore.layout === 'mix')
 const props = defineProps({
   menuList: {
     required: true,
@@ -63,3 +66,10 @@ function resolvePath(routePath) {
   return fullPath
 }
 </script>
+
+<style lang="scss" scoped>
+.left-menu-mix {
+  padding-bottom: 50px;
+  background-color: var(--menuBg);
+}
+</style>
