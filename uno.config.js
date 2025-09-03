33cd675230988ddb7f5a069b75812f6e9bb04ec5
skyselang <215817969@qq.com>
2025-09-03 13:59:52 +0800

    refactor(uno.config.js): 调整预设顺序并添加空字体配置
    将预设顺序重新排列以保持一致性，并为 presetWebFonts 添加空字体配置对象以明确初始化状态

--- uno.config.js
+++ uno.config.js
@@ -1,21 +1,30 @@
 import {
   defineConfig,
-  presetWind3,
-  presetIcons,
-  presetWebFonts,
-  presetTypography,
   presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetWebFonts,
+  presetWind3,
   transformerDirectives,
   transformerVariantGroup
 } from 'unocss'
 
 export default defineConfig({
+  shortcuts: [],
   theme: {
     colors: {
       primary: 'var(--el-color-primary)',
       primary_dark: 'var(--el-color-primary-light-5)'
     }
   },
-  presets: [presetWind3(), presetIcons(), presetWebFonts(), presetTypography(), presetAttributify()],
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {}
+    })
+  ],
   transformers: [transformerDirectives(), transformerVariantGroup()]
 })
